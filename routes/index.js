c9d0f24a24250500696ba974885f6ac2c6e849b6
eparkhurst <eli.parkhurst@gmail.com>
2016-11-09 12:49:59 -0700

    back end is set up with 2 routes, get location array and post to location array. no db is currently set up

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var bodyParser = require('body-parser')
+jsonParser = bodyParser.json()
 /* GET home page. */
 
 var locationsArray = [{lat:39.7392, lng:-104.9903},{lat:33.4484,lng: -112.0740},{lat:32.7157, lng:-117.1611}]
@@ -9,4 +10,12 @@ router.get('/', function(req, res, next) {
   res.send(locationsArray)
 });
 
+router.post('/locations', jsonParser,  function(req, res){
+  req.body.lat = parseInt(req.body.lat)
+  req.body.lng = parseInt(req.body.lng)
+  console.log(req.body);
+
+  locationsArray.push(req.body)
+  res.send(req.body)
+})
 module.exports = router;
