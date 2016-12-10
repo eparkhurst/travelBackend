d89e0965775ddbe7a85cc58c3f78d72bcbaf2f43
eparkhurst <eli.parkhurst@gmail.com>
2016-12-09 20:10:17 -0800

    added post method and broke out db queries into a seperate library

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var knex = require('../db/knex');
-
+var queries = require('../lib/dbQueries')
 /* GET home page. */
 
 
 
 router.get('/blogs', function(req, res, next) {
-  knex('locations')
-  .leftJoin('blogs','locations.id','=','blogs.location_id')
+  // knex('locations')
+  // .leftJoin('blogs','locations.id','=','blogs.location_id')
+  queries.getBlogs()
   .then(function(data){
     var toSend = data.map((e)=>{
       e.location = {
@@ -33,12 +34,22 @@ router.get('/users', function(req,res,next){
 });
 
 router.post('/locations', function(req, res){
-  req.body.lat = parseInt(req.body.lat)
-  req.body.lng = parseInt(req.body.lng)
-  console.log(req.body);
-
-  locationsArray.push(req.body)
-  res.send(req.body)
+  var obj = {}
+  obj.location={}
+  obj.location.lat = Number(req.body.lat)
+  obj.location.lng = Number(req.body.lng)
+  obj.title = req.body.title
+  obj.text = req.body.text
+  console.log(obj);
+  queries.addBlog(obj)
+    .then(function(results){
+      res.send(results)
+      return results;
+    })
+    .catch(function(err){
+      console.log('Oh NO');
+      res.json(err);
+    })
 })
 
 module.exports = router;
